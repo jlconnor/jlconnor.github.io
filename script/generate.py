87cb1dca5403039549a0cd0d6d8d6b152bb071c7
Jason L Connor <jason.connor@gmail.com>
2024-12-18 23:26:35 -0700

    Re-create the directory structure under src

--- script/generate.py
+++ script/generate.py
@@ -14,8 +14,13 @@ def main():
 
                 html_content = mistune.html(markdown_content)
 
+                relative_path = os.path.relpath(root, src_directory)
+                output_directory = os.path.join(".", relative_path)
+                os.makedirs(output_directory, exist_ok=True)
+
                 html_file_name = os.path.splitext(file)[0] + ".html"
-                with open(html_file_name, "w", encoding="utf-8") as html_file:
+                html_file_path = os.path.join(output_directory, html_file_name)
+                with open(html_file_path, "w", encoding="utf-8") as html_file:
                     html_file.write("".join(html_content))
 
 
