2fd42581eaf6fc2a8e36e0cf1bc73eb6156cbed4
Jason L Connor <jason.connor@gmail.com>
2024-12-21 09:56:05 -0700

    Add support for page titles + syntax highlighting

--- script/gen.py
+++ script/gen.py
@@ -1,9 +1,34 @@
 import os
 
 import mistune
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import get_lexer_by_name
 
 
-def convert_markdown_to_html(markdown_content):
+class PicoRenderer(mistune.HTMLRenderer):
+    def heading(self, text, level, **attrs):
+        return f'<h{level} class="h{level}">{text}</h{level}>\n'
+
+    def paragraph(self, text):
+        return f'<p class="p">{text}</p>\n'
+
+    def list(self, text, ordered, **attrs):
+        tag = "ol" if ordered else "ul"
+        return f'<{tag} class="{tag}">{text}</{tag}>\n'
+
+    def list_item(self, text):
+        return f'<li class="li">{text}</li>\n'
+
+    def block_quote(self, text, language="python"):
+        lexer = get_lexer_by_name(language)
+        formatter = HtmlFormatter()
+        highlighted_text = highlight(text, lexer, formatter)
+        return f'<blockquote class="blockquote">{highlighted_text}</blockquote>\n'
+
+
+def convert_markdown_to_html(markdown_content, title):
+    # markdown = mistune.create_markdown(renderer=PicoRenderer())
     markdown = mistune.create_markdown()
     html_body = markdown(markdown_content)
     css_url = "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
@@ -13,7 +38,8 @@ def convert_markdown_to_html(markdown_content):
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>jlconnor.github.io</title>
+        <meta name="color-scheme" content="light dark">
+        <title>jlconnor.github.io: {title}</title>
         <link rel="stylesheet" href="{css_url}">
     </head>
     <body>
@@ -37,7 +63,8 @@ def process_markdown_files(input_directory, output_directory):
                 )
                 with open(input_path, "r", encoding="utf-8") as md_file:
                     markdown_content = md_file.read()
-                html_content = convert_markdown_to_html(markdown_content)
+                title = os.path.splitext(filename)[0]
+                html_content = convert_markdown_to_html(markdown_content, title=title)
                 with open(output_path, "w", encoding="utf-8") as html_file:
                     html_file.write(html_content)
                 print(f"Converted {os.path.join(rel_path, filename)} to HTML.")
